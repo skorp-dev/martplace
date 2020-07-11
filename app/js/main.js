89fdf27d6432732393ce255dda955c5d61c36a15
skorp-dev <skorpwebx@gmail.com>
2020-07-11 07:00:26 +0200

    added weekly section

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+
+    $('.weekly__slider').slick({
+        arrows: true,
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/weekly-next.svg" alt=""></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/weekly-prev.svg" alt=""></button>',
+    });
+});
