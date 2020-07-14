d549a5487384507f13af465a11fac318ba4672da
skorp-dev <skorpwebx@gmail.com>
2020-07-14 14:16:12 +0200

    added products

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,17 @@ $(function () {
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/weekly-next.svg" alt=""></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/weekly-prev.svg" alt=""></button>',
     });
+
+    var mixer = mixitup('.product');
+
+    
+$('.products__filter').on('click', function(){
+    var $dropMenu = $(this).parent().find('.products__filter-menu');
+    if($dropMenu.is(':visible')){
+        $dropMenu.fadeOut();
+    } else {   
+        $('.dropMenu:visible').fadeOut(); 
+        $dropMenu.fadeIn();
+    }
+});
 });
