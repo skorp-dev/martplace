5e8e13c7d28eb659ae657959d2169217f26278e3
skorp-dev <skorpwebx@gmail.com>
2020-07-18 13:09:05 +0200

    fixed styles

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,36 @@ $(function () {
 
     $('.weekly__slider').slick({
         arrows: true,
-        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/weekly-next.svg" alt=""></button>',
-        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/weekly-prev.svg" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+        appendArrows: '.weekly__arrows'
     });
 
-    var mixer = mixitup('.product');
+    var mixer = mixitup('.products__inner');
 
     
-$('.products__filter').on('click', function(){
-    var $dropMenu = $(this).parent().find('.products__filter-menu');
-    if($dropMenu.is(':visible')){
-        $dropMenu.fadeOut();
-    } else {   
-        $('.dropMenu:visible').fadeOut(); 
-        $dropMenu.fadeIn();
-    }
-});
+ $('.products__filter').on('click', function(){
+     var $dropMenu = $(this).parent().find('.products__filter-menu');
+     if($dropMenu.is(':visible')){
+         $dropMenu.fadeOut();
+     } else {   
+         $('.dropMenu:visible').fadeOut(); 
+         $dropMenu.fadeIn();
+     }
+ });
+
+ $(".rate-star").rateYo({
+    starWidth: "17px",
+    spacing: "5px",
+    ratedFill: "#ffc000",
+    readOnly: true
+  });
+
+ $(".rate-star--small").rateYo({
+    starWidth: "12px",
+    spacing: "3px",
+    ratedFill: "#ffc000",
+    readOnly: true
+  });
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,56 +2,68 @@ let gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync')
-    autoprefixer = require('gulp-autoprefixer'),
+autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin');
 
-gulp.task('sass', function(){
-   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 8 versions']
-    }))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 versions']
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('style', function(){
+gulp.task('style', function () {
     return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
-    ])
-    .pipe(concat('libs.min.css'))
-    .pipe(cssmin())
-    .pipe(gulp.dest('app/css'))
+            'node_modules/normalize.css/normalize.css',
+            'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+            'node_modules/rateyo/src/jquery.rateyo.css'
+        ])
+        .pipe(concat('libs.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('app/css'))
 });
 
-gulp.task('script', function(){
+gulp.task('script', function () {
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
-    ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/rateyo/src/jquery.rateyo.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
 });
 
-gulp.task('html', function(){
+gulp.task('html', function () {
     return gulp.src('app/*.html')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('js', function(){
+gulp.task('js', function () {
     return gulp.src('app/js/*.js')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 
-gulp.task('browser-sync', function(){
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "app/"
@@ -59,7 +71,7 @@ gulp.task('browser-sync', function(){
     });
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'));
