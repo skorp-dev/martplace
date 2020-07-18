d7a2596f50bde35a7af5503cea3f37ef4e0fa5d8
skorp-dev <skorpwebx@gmail.com>
2020-07-18 18:26:39 +0200

    fixed styles html code

--- app/js/main.js
+++ app/js/main.js
@@ -4,34 +4,34 @@ $(function () {
         arrows: true,
         prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
         nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
-        appendArrows: '.weekly__arrows'
+        appendArrows: '.top-box__arrows'
     });
 
     var mixer = mixitup('.products__inner');
 
-    
- $('.products__filter').on('click', function(){
-     var $dropMenu = $(this).parent().find('.products__filter-menu');
-     if($dropMenu.is(':visible')){
-         $dropMenu.fadeOut();
-     } else {   
-         $('.dropMenu:visible').fadeOut(); 
-         $dropMenu.fadeIn();
-     }
- });
 
- $(".rate-star").rateYo({
-    starWidth: "17px",
-    spacing: "5px",
-    ratedFill: "#ffc000",
-    readOnly: true
-  });
+    $('.top-box__filter-btn').on('click', function () {
+        var $dropMenu = $(this).parent().find('.top-box__filter-menu');
+        if ($dropMenu.is(':visible')) {
+            $dropMenu.fadeOut();
+        } else {
+            $('.dropMenu:visible').fadeOut();
+            $dropMenu.fadeIn();
+        }
+    });
 
- $(".rate-star--small").rateYo({
-    starWidth: "12px",
-    spacing: "3px",
-    ratedFill: "#ffc000",
-    readOnly: true
-  });
+    $(".rate-star").rateYo({
+        starWidth: "17px",
+        spacing: "5px",
+        ratedFill: "#ffc000",
+        readOnly: true
+    });
+
+    $(".rate-star--small").rateYo({
+        starWidth: "12px",
+        spacing: "3px",
+        ratedFill: "#ffc000",
+        readOnly: true
+    });
 
 });
